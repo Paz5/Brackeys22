eb2678e94a267ef53e6d1bcc1487f506878722e1
GoDArCiO <jalochaartur1337@gmail.com>
2022-02-20 20:05:52 +0100

    highlight and loop

---
+++ Assets/Scripts/RepairBird/DamagedBird.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagedBird : MonoBehaviour
+{
+    public PartType battery;
+    public PartType camera;
+    public PartType wings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
