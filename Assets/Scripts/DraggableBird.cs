9c54938ba7d63479424f4c3f8ba72aa6d9c19d02
Tymon Brzozowski <tybrz10@outlook.com>
2022-02-21 15:01:21 +0100

    Odlatywanie przy failu

--- Assets/Scripts/DraggableBird.cs
+++ Assets/Scripts/DraggableBird.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using ScriptableObjectArchitecture;
 using UnityEngine;
 using UnityEngine.Pool;
@@ -15,12 +16,14 @@ public class DraggableBird : MonoBehaviour{
     private Plane dragPlane;
     private Vector3 dragStartPos = Vector3.zero;
     private bool alreadySpawned = false;
+    private bool draggable = true;
 
     private void OnEnable(){
         if (alreadySpawned){
             transform.position = manager.GetWaitPos();
             smoothFollow.SetPosition(manager.GetSpawnPos());
             correctPosition = false;
+            draggable = true;
             dragStartPos = Vector3.zero;
         }
     }
@@ -37,27 +40,44 @@ public class DraggableBird : MonoBehaviour{
     }
 
     public void MoveToMouse(){
-        Ray ray = screen.camera.ViewportPointToRay(screen.pointerPos.Value);
-        dragPlane.Raycast(ray, out float enter);
-        Vector3 pos = enter * ray.direction + screen.camera.transform.position;
-        transform.position =  pos;
+        if (draggable){
+            Ray ray = screen.camera.ViewportPointToRay(screen.pointerPos.Value);
+            dragPlane.Raycast(ray, out float enter);
+            Vector3 pos = enter * ray.direction + screen.camera.transform.position;
+            transform.position =  pos;
+        }
     }
 
     public void EndDrag(){
-        if (correctPosition){
-            succesEvent.Raise();
-            pool.Release(gameObject);
-        }
-        else{
-            failEvent.Raise();
-            transform.position = dragStartPos;
+        if (draggable){
+            if (correctPosition){
+                succesEvent.Raise();
+                transform.position = targetPos;
+            }
+            else{
+                Fail();
+                transform.position += Vector3.up * 10f;
+            }
+            StartCoroutine(Disappear());
+            draggable = false;
         }
     }
 
+    private void Fail(){
+        failEvent.Raise();
+    }
+
+    IEnumerator Disappear(){
+        yield return new WaitForSeconds(1f);
+        pool.Release(gameObject);
+    }
+
     private bool correctPosition = false;
+    private Vector3 targetPos = Vector3.zero;
     
     private void OnTriggerEnter(Collider other){
         correctPosition = true;
+        targetPos = other.transform.position;
     }
 
     private void OnTriggerExit(Collider other){
